65f48307c52fbe55756baabe2df5580f8fe06c36
monster-hui <373000945@qq.com>
2016-05-17 16:09:10 +0800

    change
    add

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,20 @@
 'use strict';
 
 function thousands_separators(num) {
-  
+  if(isNaN(num)){
+		alert( "illegal input");
+		return num;
+		}
+	 num+='';
+	 numArray= num.split('.');
+	 numLeft = numArray[0];
+	 numbRight = numArray.length > 1 ? '.' + numArray[1] : '';
+	 var form = /(\d+)(\d{3})/;
+	 while (form.test(numLeft)) {
+	  numLeft = numLeft.replace(form, '$1' + ',' + '$2');
+	 }
+	var  outs= numLeft + numbRight;
+	return outs;
 }
 
 module.exports = thousands_separators;
